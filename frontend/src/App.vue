05e1ce1f201e8330d7de01fe0eb055774f3cbfcf
Kei <kei.n140802@gmail.com>
2026-02-04 10:12:30 +0900

    finish tutorial

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import TestComponent from './components/TestComponent.vue'
 
 const message = ref('')
 
@@ -8,10 +9,23 @@ onMounted(async () => {
   const data = await res.json()
   message.value = data.message
 })
+
+const data = [
+  { name: '親コンポーネントから渡されたデータ', id: 0 }
+];
+
+const receivedMessage = ref('');
+const handleMessage = (mes) => {
+  receivedMessage.value = mes;
+}
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <div>
+    <h1>{{ message }}</h1>
+    <TestComponent :text="data[0].name" @message="handleMessage"/>
+    <p>{{ receivedMessage }}</p>
+  </div>
 </template>
 
 <style scoped>
