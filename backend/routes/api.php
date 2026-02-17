7747ac15c3add9a744d12d61be495c38b58db8e2
Kei <kei.n140802@gmail.com>
2026-02-17 16:49:58 +0900

    sanctumのインストール、設定

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,11 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TodoController;
+use Illuminate\Http\Request;
 
 Route::get('/hello', function () {
   return ['message' => 'Hello from API'];
 });
 
+// 認証用テストルート
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return response()->json([
+        'message' => '認証成功',
+        'user' => $request->user(),
+    ]);
+});
+
+// Todo画面
 Route::get('/todos', [TodoController::class, 'index']);
 Route::get('todos/{id}', [TodoController::class, 'show']);
 Route::post('/todos', [TodoController::class, 'store']);
