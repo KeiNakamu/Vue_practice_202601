7113621fc61372d8f79c1fb4f0fa1370290bda91
Kei <kei.n140802@gmail.com>
2026-02-19 11:43:27 +0900

    エラーメッセージを日本語化

---
+++ backend/lang/ja/validation.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'required' => ':attribute は必須です。',
+    'email' => ':attribute の形式が正しくありません。',
+
+    'attributes' => [
+        'name' => '名前',
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+    ],
+
+];
